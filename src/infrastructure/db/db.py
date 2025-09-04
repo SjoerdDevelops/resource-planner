643db258784cb4c481551ea4a435cd8d57272227
sjoerddevelops@gmail.com <skuitert@HIT-015.heemtech.nl>
2025-09-04 13:53:30 +0200

    Added functionality to create databases for different environments

--- src/infrastructure/db/db.py
+++ src/infrastructure/db/db.py
@@ -1,10 +1,20 @@
 from peewee import SqliteDatabase
 from infrastructure.models import EmployeeModel
+from enum import Enum
 
-db = SqliteDatabase("database.db")
+
+class Environment(str, Enum):
+    DEV = ":memory:"
+    TEST = ":memory:"
+    PROD = "database.db"
+
+
+def create_db(environment: Environment):
+    return SqliteDatabase(environment)
 
 
 def init_db():
+    db = create_db(Environment.DEV)
     db.bind([EmployeeModel])
     db.connect()
     db.create_tables([EmployeeModel])
