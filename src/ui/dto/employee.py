262916469af2983697e2505df73c692cfb8a08bc
sjoerddevelops@gmail.com <skuitert@HIT-015.heemtech.nl>
2025-09-09 11:07:45 +0200

    Added validation and normalization to the employee dtos
    Data of every string field in the employee dtos is normalized to
    standard formatting. Validation is handled using pydantic
    field_validators. Validators return a human readable string that can be
    used in the UI layer to provide feedback to users.

--- src/ui/dto/employee.py
+++ src/ui/dto/employee.py
@@ -1,28 +1,92 @@
-from dataclasses import dataclass
-from uuid import UUID
+from pydantic import (
+    BaseModel,
+    UUID4,
+    field_validator,
+    ValidatorInfo,
+)
 
 
-@dataclass
-class PersonalInfoDTO:
+class PersonalInfoDTO(BaseModel):
     name: str
     surname: str
 
+    @field_validator("name", "surname")
+    def validate_and_normalize_name(cls, value: str, info: "ValidatorInfo") -> str:
+        value.strip().title()
 
-@dataclass
-class EmploymentDetailsDTO:
+        field_name = info.field_name.capitalize()
+
+        if len(value) < 1:
+            raise ValueError(f"{field_name} cannot be empty")
+
+        if len(value) > 50:
+            raise ValueError(f"{field_name} cannot exceed 50 characters")
+
+        if not all(char.isalpha() or char.isspace() for char in value):
+            raise ValueError(f"{field_name} must contain only letters and spaces")
+
+        return value
+
+
+class EmploymentDetailsDTO(BaseModel):
     fte: float
     utilization_rate: float
 
+    @field_validator("fte", "utilization_rate")
+    def validate_employment_details(cls, value: float, info: "ValidatorInfo") -> float:
+        field_name = info.field_name.capitalize()
 
-@dataclass
-class CompanyCredentialsDTO:
+        if value < 0 or value > 1:
+            raise ValueError(f"{field_name} must be between 0.0 and 1.0")
+
+        return value
+
+
+class CompanyCredentialsDTO(BaseModel):
     username: str
     acronym: str
 
+    @field_validator("username")
+    def validate_and_normalize_username(cls, value: str, info: "ValidatorInfo") -> str:
+        value.strip().lower()
 
-@dataclass
-class EmployeeDTO:
-    id: UUID
+        field_name = info.field_name.capitalize()
+        min_length = 3
+        max_length = 20
+
+        if len(value) < 1:
+            raise ValueError(f"{field_name} cannot be empty")
+
+        if len(value) < min_length:
+            raise ValueError(f"{field_name} must be atleast {min_length} characters")
+
+        if len(value) > max_length:
+            raise ValueError(f"{field_name} cannot exceed {max_length} characters")
+
+        if not all(char.isalpha() for char in value):
+            raise ValueError(f"{field_name} must contain only letters")
+
+        return value
+
+    # Normalize acronym to upper case
+    @field_validator("acronym")
+    def validate_and_normalize_acronym(cls, value: str, info: "ValidatorInfo") -> str:
+        value.strip().upper()
+
+        field_name = info.field_name.capitalize()
+        acronym_length = 3
+
+        if len(value) != acronym_length:
+            raise ValueError(f"{field_name} must be {acronym_length} characters")
+
+        if not all(char.isalpha() for char in value):
+            raise ValueError(f"{field_name} must contain only letters")
+
+        return value
+
+
+class EmployeeDTO(BaseModel):
+    id: UUID4
     personal: PersonalInfoDTO
     employment: EmploymentDetailsDTO
     credentials: CompanyCredentialsDTO
